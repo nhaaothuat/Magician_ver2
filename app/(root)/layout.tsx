63f14ccfb56100cec46128e2bbd77a958616476f
Kitty <hienphannguyenxuan@gmail.com>
2024-03-05 01:40:34 +0700

    Nice

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import MobileNav from "@/components/shared/MobileNav";
 import SideBar from "@/components/shared/Sidebar";
-
+import { Toaster } from "@/components/ui/toaster";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
@@ -12,6 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
+      <Toaster />
     </main>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
+      
         </body>
       </html>
     </ClerkProvider>
